f530136fa6800e62c870e5a71c4259d21df36140
Will Speak <lithiumflame@gmail.com>
2015-12-25 22:04:55 +0000

    AST Implementation for Rust Port of Prattle

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,125 @@
-#![cfg_attr(test, feature(test))]
-#![feature(plugin)]
-#![plugin(peg_syntax_ext)]
-
-peg_file! parser("grammar.peg"); // Fix " " .
-
-#[cfg(not(test))]
-fn dump_parse(s: &str) -> Result<(),parser::ParseError> {
-    let words = try!(parser::words(s));
-    println!("{} = [{}]", s, words.join(", "));
-    Ok(())
+/// Operator
+///
+/// Represents an operation that can be applied to an expression
+/// either as a prefix operation or as an infix binary operation.
+#[derive(Debug, PartialEq)]
+enum Operator {
+    Add,
 }
 
-#[cfg(not(test))]
-fn main() {
-    dump_parse("hello world").unwrap();
+/// Expression
+///
+/// A single Expression node. Represents either a value or complex
+/// expression.
+#[derive(Debug)]
+enum Expression<'a> {
+    /// Value Expression
+    ///
+    /// Represents a simple numeric value
+    ValueExpression(i32),
+
+    /// Variable Expression
+    ///
+    /// Represents a reference to a variable
+    VariableExpression(String),
+
+    /// Prefix Operator Expression
+    ///
+    /// Represents a unary prefix operator
+    PrefixOperatorExpression {
+        operator: Operator,
+        expression: &'a Expression<'a>,
+    },
+
+    /// Binary Operator Expression
+    ///
+    /// Represents a binary operator.
+    BinaryOperatorExpression {
+        lhs: &'a Expression<'a>,
+        operator: Operator,
+        rhs: &'a Expression<'a>,
+    },
 }
 
 #[cfg(test)]
 mod test {
-    extern crate test;
 
-    use self::test::Bencher;
-
-    use super::parser::*;
+    use super::Expression::*;
+    use super::Operator;
 
     #[test]
-    fn test_comments() {
-        let parsed = words("// this is a comment").unwrap();
-        assert_eq!(0, parsed.len());
-    }
-
-    #[test]
-    fn test_single_word() {
-        let parsed = words("w").unwrap();
-        assert_eq!("w", parsed[0]);
+    pub fn test_create_value_expression() {
+        let expr = ValueExpression(32);
+        match expr {
+            ValueExpression(value) => assert_eq!(value, 32),
+            _ => assert!(false),
+        }
     }
 
     #[test]
-    fn test_multiple_words() {
-        let parsed = words("some words to parse").unwrap();
-        assert_eq!("some", parsed[0]);
-        assert_eq!("words", parsed[1]);
-        assert_eq!("to", parsed[2]);
-        assert_eq!("parse", parsed[3]);
+    pub fn test_create_variable_expression() {
+        let expr = VariableExpression("hello".to_string());
+        match expr {
+            VariableExpression(name) => assert_eq!(name, "hello".to_string()),
+            _ => assert!(false),
+        }
     }
 
-    #[bench]
-    fn bench(b: &mut Bencher) {
-        b.iter(|| {
-            words("Nulla facilisiFusce ut euismod urna ut bibendum nibh
-Nam leo urna suscipit sed lorem non vestibulum mattis mauris
-Pellentesque commodo porta nulla
-//Phasellus fringilla metus metus nec elementum risus euismod at
-Phasellus porttitor felis risus et scelerisque enim fringilla in
-Nullam vel vehicula nibh
-Quisque pellentesque fringilla ante non porta
-Donec neque diam luctus auctor ante in consectetur feugiat ante
-Donec malesuada ante in tellus porttitor at imperdiet lorem pulvinar
-//Nullam vitae purus finibus tristique ante ut dapibus velit
-//In eget mollis sapien sed accumsan velit
-Curabitur aliquet sapien a turpis lobortis dapibus a ut dolor
-//Ut accumsan ligula risus vel sodales lacus sollicitudin eu.
-//
-").unwrap();
-        });
+    #[test]
+    pub fn test_create_prefix_operation_expression() {
+        let value_expr = ValueExpression(64);
+        let expr = PrefixOperatorExpression {
+            operator: Operator::Add,
+            expression: &value_expr,
+        };
+        match expr {
+            PrefixOperatorExpression{ operator, expression } => {
+                assert_eq!(Operator::Add, operator);
+                if let &ValueExpression(value) = expression {
+                    assert_eq!(64, value)
+                } else {
+                    assert!(false);
+                }
+            }
+            _ => assert!(false),
+        }
     }
+
+    #[test]
+    pub fn test_create_binary_operator_expression() {
+        let lhs_expr = ValueExpression(111);
+        let rhs_expr = ValueExpression(222);
+        let expr = BinaryOperatorExpression {
+            lhs: &lhs_expr,
+            operator: Operator::Add,
+            rhs: &rhs_expr,
+        };
+        match expr {
+            BinaryOperatorExpression { lhs, operator, rhs } => {
+                match lhs {
+                    &ValueExpression(value) => assert_eq!(value, 111),
+                    _ => assert!(false),
+                };
+                match rhs {
+                    &ValueExpression(value) => assert_eq!(value, 222),
+                    _ => assert!(false),
+                };
+                assert_eq!(operator, Operator::Add)
+            }
+            _ => assert!(false),
+        }
+    }
+
+}
+
+fn main() {
+    let foo = Expression::BinaryOperatorExpression {
+        lhs: &Expression::ValueExpression(123),
+        operator: Operator::Add,
+        rhs: &Expression::PrefixOperatorExpression {
+            operator: Operator::Add,
+            expression: &Expression::VariableExpression("test".to_string()),
+        },
+    };
+    println!("{:?}", foo);
 }
