9d9585cf243123b56cd9b6d08a2083755f30d315
Will Speak <lithiumflame@gmail.com>
2016-06-12 10:11:50 +0100

    Simplify Parser Implementation
    Completes the reorganisation of the parser and expression tree. Parsing
    is now self-contained in a single file.
    
    Parsing has been moved away from using the parselet structs and traits
    along with their related parselet data types and directly into method
    implementations for `Token`. These methods are private to the `parse`
    module so shouldn't interferere with anything.
    
    The parser itself has been reworked to allow tokens to easily call back
    in to advance the iterator and perform look-ahead checks.
    
    Still TODO:
    
      * Update the error struct to be more descriptive.
      * Update the expression struct to contain metadata about the span of
       	the expression.
      * Update tokens to split them into two sets: basic lexical tokens and
      	trivia tokens.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,91 @@
-mod parse;
-mod token;
-mod tokeniser;
-mod expression;
+//! Expression tree parsing using Top-Down Operator Precedence
+//! parsing.
+//!
+//! This crate provides a simple abstract syntax tree, and a parser
+//! implementation which recognises a simple lanugage using
+//! Pratt-style operator precedence parsing.
 
-pub use parse::{Parser,Error as ParseError,Result as ParseResult};
-pub use token::Token;
-pub use tokeniser::Tokeniser;
-pub use expression::{Expression,Operator};
+mod parse;
+
+/// This structure represents a single token from the input source
+/// buffer.
+#[derive(Debug,PartialEq)]
+pub enum Token {
+    /// Represents a string of alphabetic characters. This could be a
+    /// language keyword or a variable or type identifier.
+    Word(String),
+
+    /// Whitespace trivia
+    Whitespace(String),
+
+    /// Constant numerical value.
+    Literal(i64),
+
+    /// The `=` character
+    Equals,
+
+    /// The `+` character
+    Plus,
+
+    /// The `-` character
+    Minus,
+
+    /// The `*` character
+    Star,
+
+    /// The `/` character
+    Slash,
+
+    /// The `(` character
+    OpenBracket,
+
+    /// The `)` character
+    CloseBracket,
+
+    /// The `,` character
+    Comma,
+}
+
+/// Represents an AST prefix operator.
+#[derive(Debug,PartialEq)]
+pub enum PrefixOp {
+    Negate,
+}
+
+/// Represents an AST infix operator
+#[derive(Debug,PartialEq)]
+pub enum InfixOp {
+    Assign,
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl InfixOp {
+    fn for_token(tok: &Token) -> Option<Self> {
+        use InfixOp::*;
+        use Token::*;
+        match *tok {
+            Equals => Some(Assign),
+            Star => Some(Mul),
+            Slash => Some(Div),
+            Plus => Some(Add),
+            Minus => Some(Sub),
+            _ => None,
+        }
+    }
+}
+
+/// Represents an AST expression.
+#[derive(Debug,PartialEq)]
+pub enum Expression {
+    Identifier(String),
+    Literal(i64),
+    Prefix(PrefixOp, Box<Expression>),
+    Infix(Box<Expression>, InfixOp, Box<Expression>),
+    Call(Box<Expression>, Vec<Expression>),
+}
 
 #[cfg(not(test))]
 fn main() {
@@ -16,9 +95,9 @@ fn main() {
     let stdin = io::stdin();
     for line_io in stdin.lock().lines() {
         if let Ok(line) = line_io {
-            match Expression::parse(&line) {
-                Ok(parsed) => println!("OK > {}", parsed),
-                Err(err) => println!("Error: {:?}", err)
+            match Expression::parse_str(&line) {
+                Ok(parsed) => println!("OK > {:?}", parsed),
+                Err(err) => println!("Error: {:?}", err),
             };
         };
     }
--- src/parse.rs
+++ src/parse.rs
@@ -1,212 +1,341 @@
+//! Contains the `Tokeniser` and `Parser` structs. These are
+//! responsible for parsing a buffer into an expression tree.
+
 use std::iter::Peekable;
 
-use super::{Expression,Operator,Token,Tokeniser};
+use super::{Token, Expression, PrefixOp, InfixOp};
 
-/// Parser Result
-pub type Result<T> = ::std::result::Result<T, Error>;
+pub use self::error::{Error, Result};
 
-/// Parse Error
-#[derive(Debug)]
-pub enum Error {
-    /// Incomplete
-    ///
-    /// There weren't enough tokens to complete parsing the
-    /// expression.
-    Incomplete,
+impl Expression {
 
-    /// Unexpected Token
-    ///
-    /// Signals that there was a token in the token stream which
-    /// couldn't be parsed in that position. E.g. when we are
-    /// expecting a closnig brace but get another token
-    /// instead. Could also be that we were expecting a token but
-    /// didn't get one either, hence the Option<Token>`
-    Unexpected {
-        found: Option<Token>,
-        expecting: String,
-    },
+    /// Parse expression from string. Takes a reference to an
+    /// expression and returns a result containing the parsed
+    /// expression, or an error if none could be parsed.
+    pub fn parse_str(s: &str) -> Result<Expression> {
+        let t = Tokeniser::new_from_str(s);
+        let mut p = Parser::new(t);
+        p.expression(0)
+    }
 }
 
-/// Infix Parselet Data
+
+/// Tokeniser
 ///
-/// The data required to control the parsing of a given type of
-/// infix parselet.
-enum InfixParseletData {
-
-    /// Binary Operator
-    BinOp(Operator),
-
-    /// Function Call
-    FnCall
+/// An object which can run a regex state machine over an input
+/// buffer, producing tokens when each new lexeme is matched.
+struct Tokeniser {
+    buff: String,
+    idx: usize,
 }
 
-/// Infix Parselet
-///
-/// An infix parselet is an object capable of parsing the trailing
-/// part of a given expression. It contains a binding power which
-/// expresses associativity and parse data which controls the way
-/// the expresison is parsed.
-///
-/// This parselet consumes operators and expressions which follow
-/// a primary expression. This can be a chain of operators at the
-/// same precedence level, nested expressions at a lower
-/// precedence level, or a more complex expression such as a
-/// function call or index expression.
-struct InfixParselet {
+impl Tokeniser {
+    /// Creates a new tokeniser from the given string slice.
+    pub fn new_from_str(source: &str) -> Tokeniser {
+        Tokeniser {
+            buff: source.to_string(),
+            idx: 0,
+        }
+    }
 
-    /// Binding Power
-    ///
-    /// How tightly the parselet associates itself with the
-    /// expression to the left.
-    pub binding_power: i32,
+    fn next_raw(&mut self) -> Option<Token> {
 
-    /// Parse State
-    ///
-    /// Private state for use by the parselet implementaiton
-    parse_state: InfixParseletData
-}
-
-impl InfixParselet {
-
-    /// Parse
-    ///
-    /// Attempt to parse an expression given the current parser
-    /// state and expression. This should only be called when the
-    /// next token is known to be the first token of this infix
-    /// parselet.
-    ///
-    /// The resulting expression should contain `lhs` wrapped in
-    /// one or more operators.
-    pub fn parse(&self, parser: &mut Parser, lhs: Expression) -> Result<Expression> {
-        match self.parse_state {
-            InfixParseletData::BinOp(op) => {
-                let rhs = try!(parser.parse(self.binding_power));
-                Ok(Expression::from_binary_op(lhs, op, rhs))
-            }
-            InfixParseletData::FnCall => {
-                // collect up the parameters into a vector
-                let mut params = Vec::new();
-                loop {
-                    match parser.parse(0) {
-                        Ok(param) => params.push(param),
-                        Err(Error::Incomplete) => return Err(Error::Incomplete),
-                        _ => break
-                    }
+        let ts = self.idx;
+        let mut te = ts;
+        let mut chars = self.buff[ts..].chars();
+        let tok = chars.next().and_then(|c| {
+            te += 1;
+            match c {
+                '=' => Some(Token::Equals),
+                '+' => Some(Token::Plus),
+                '-' => Some(Token::Minus),
+                '*' => Some(Token::Star),
+                '/' => Some(Token::Slash),
+                '(' => Some(Token::OpenBracket),
+                ')' => Some(Token::CloseBracket),
+                ',' => Some(Token::Comma),
+                '0'...'9' => {
+                    te += chars.take_while(|c| *c >= '0' && *c <= '9').count();
+                    let token_str = &self.buff[ts..te];
+                    // we have cheked that it's a valid numeric literal,
+                    // so unwrap is fine here.
+                    Some(Token::Literal(token_str.parse::<i64>().unwrap()))
                 }
-                // call should be terminated by a closing bracket
-                try!(parser.expect(Token::CloseBracket));
-                Ok(Expression::from_function_call(lhs, params))
+                c if c.is_alphabetic() || c == '_' => {
+                    te += chars.take_while(|c| c.is_alphanumeric() || *c == '_')
+                               .count();
+                    Some(Token::Word(self.buff[ts..te].to_string()))
+                }
+                c if c.is_whitespace() => {
+                    te += chars.take_while(|c| c.is_whitespace())
+                               .count();
+                    Some(Token::Whitespace(self.buff[ts..te].to_string()))
+                }
+                _ => None,
+            }
+        });
+        self.idx = te;
+        tok
+    }
+}
+
+/// Tokeniser Iterator implementation.
+///
+/// This allows iterator adapters to be used with the token
+/// stream. The next method filters out whitespace tokens from the
+/// tokeniser too. This means the `Tokeniser` doesn't have to worry
+/// about skipping certain lexemes in the grammar.
+impl Iterator for Tokeniser {
+    type Item = Token;
+
+    /// Iterator next method. This method returns the next
+    /// non-whitespace token in the `Tokeniser`'s stream of `Token`s.
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(tok) = self.next_raw() {
+            if let Token::Whitespace(_) = tok {
+            } else {
+                return Some(tok);
             }
         }
-    }
-
-    /// Bin Op
-    ///
-    /// Creates a new InfixParselet which will consume a given
-    /// binary operator at a given precendence level.
-    pub fn bin_op(binding_power: i32, op: Operator) -> Self {
-        InfixParselet {
-            binding_power: binding_power,
-            parse_state: InfixParseletData::BinOp(op),
-        }
-    }
-    
-    /// Function Call
-    ///
-    /// Creates a new InfixParseet which will consume a function
-    /// call expression.
-    pub fn fn_call() -> Self {
-        InfixParselet {
-            binding_power: 8,
-            parse_state: InfixParseletData::FnCall
-        }
+        None
     }
 }
 
-/// Get Infix Parselet
-///
-/// Looks up an infix parselet, if one exists, for a given token.
-fn get_infix_parselet(token: Option<&Token>) -> Option<InfixParselet> {
-    match token {
-        Some(&Token::Equals) => Some(InfixParselet::bin_op(1, Operator::Assign)),
-        Some(&Token::Plus) => Some(InfixParselet::bin_op(2, Operator::Add)),
-        Some(&Token::Minus) => Some(InfixParselet::bin_op(2, Operator::Sub)),
-        Some(&Token::Star) => Some(InfixParselet::bin_op(3, Operator::Mul)),
-        Some(&Token::Slash) => Some(InfixParselet::bin_op(3, Operator::Div)),
-        Some(&Token::OpenBracket) => Some(InfixParselet::fn_call()),
-        _ => None,
-    }
-}
-
-/// Parser
-///
-/// Represents the state required when parsing an expression
-pub struct Parser {
-    ts: Peekable<Tokeniser>,
+/// Expression parser. Given a stream of tokens this will produce
+/// an expression tree, or a parse error.
+struct Parser {
+    lexer: Peekable<Tokeniser>,
 }
 
 impl Parser {
-    /// Create a New Parser
-    ///
-    /// Initialises a new parser from a given token stream.
-    pub fn new(tokens: Tokeniser) -> Parser {
-        Parser { ts: tokens.peekable() }
+    pub fn new(t: Tokeniser) -> Parser {
+        Parser { lexer: t.peekable() }
     }
 
-    /// Parse
-    ///
-    /// Parses an expression from the input and returns it.
-    pub fn parse(&mut self, binding_power: i32) -> Result<Expression> {
-        let mut lhs = try!(self.parse_prefix_expression());
-
-        let mut maybe_parselet = get_infix_parselet(self.ts.peek());
-
-        while let Some(parselet) = maybe_parselet {
-            if binding_power >= parselet.binding_power {
-                break;
+    /// Moves the token stream on by a single token
+    fn advance(&mut self) -> Result<()> {
+        match self.lexer.peek() {
+            Some(_) => {
+                self.lexer.next();
+                Ok(())
             }
-
-            // advance the token stream past the token we used to
-            // find the infix parselet.
-            self.ts.next();
-
-            lhs = try!(parselet.parse(self, lhs));
-
-            maybe_parselet = get_infix_parselet(self.ts.peek());
+            _ => Err(Error::Incomplete),
         }
-
-        Ok(lhs)
     }
 
-    /// Expect
-    ///
-    /// Advance the token stream, returning an error if the token is
-    /// not of the expeted type.
-    pub fn expect(&mut self, expected: Token) -> Result<Token> {
-        let expecting = format!("{:?}", expected);
-        self.ts.next()
-            .map_or(
-                Err(Error::Unexpected {
-                    expecting: expecting.to_string(),
-                    found: None
-                }),
-                |t| {
-                    if t == expected {
-                        Ok(t)
-                    } else {
-                        Err(Error::Unexpected{
-                            expecting: expecting.to_string(),
-                            found: Some(t),
-                        })
-                    }
-                })
+    /// Moves the token stream on by a single token, if the
+    /// token's lexeme is of the given type.
+    pub fn advance_if(&mut self, expected: Token) -> Result<()> {
+        if !self.next_is(expected) {
+            return Err(Error::Unexpected);
+        }
+        self.advance()
     }
 
-    fn parse_prefix_expression(&mut self) -> Result<Expression> {
+    /// Checks that the next token is of the given type
+    pub fn next_is(&mut self, expected: Token) -> bool {
+        self.lexer.peek() == Some(&expected)
+    }
 
-        match self.ts.next() {
-            Some(token) => token.prefix(self),
+    /// Attempt to parse a single expression
+    pub fn expression(&mut self, rbp: u32) -> Result<Expression> {
+        let mut left = try!(self.parse_nud());
+        while self.next_binds_tighter_than(rbp) {
+            left = try!(self.parse_led(left));
+        }
+        Ok(left)
+    }
+
+    /// Returns true if the next token's lbp is > the given rbp
+    fn next_binds_tighter_than(&mut self, rbp: u32) -> bool {
+        match self.lexer.peek() {
+            Some(ref token) => token.lbp() > rbp,
+            None => false,
+        }
+    }
+
+    /// Attempt to parse a single left denotation
+    fn parse_led(&mut self, lhs: Expression) -> Result<Expression> {
+        match self.lexer.next() {
+            Some(token) => token.led(self, lhs),
             None => Err(Error::Incomplete),
         }
+    }
+
+    /// Attempt to parse a single null denotation
+    fn parse_nud(&mut self) -> Result<Expression> {
+        match self.lexer.next() {
+            Some(token) => token.nud(self),
+            None => Err(Error::Incomplete),
+        }
+    }
+}
+
+impl Token {
+
+    /// Left binding power. This controls the precedence of
+    /// the symbol when being parsed as an infix operator.
+    ///
+    /// Returns the associativity, or binding power, for the given
+    /// token. This is used when deciding if to parse the `led()`
+    /// of this token.
+    fn lbp(&self) -> u32 {
+        match *self {
+            Token::Equals => 10,
+
+            Token::Plus | Token::Minus => 50,
+
+            Token::Star | Token::Slash => 60,
+
+            Token::OpenBracket => 80,
+
+            _ => 0,
+        }
+    }
+
+    /// Null denotation. This is the parse of the symbol when it
+    /// doesn't have any expression to the left hand side of it.
+    ///
+    /// This is responsible for parsing literals and variable
+    /// references into expressions, as well as parsing prefix
+    /// expressions
+    fn nud(&self, parser: &mut Parser) -> Result<Expression> {
+        match *self {
+            Token::Word(ref word) => Ok(Expression::Identifier(word.clone())),
+            Token::Literal(i) => Ok(Expression::Literal(i)),
+            Token::Plus => parser.expression(100),
+            Token::Minus => {
+                let rhs = try!(parser.expression(100));
+                Ok(Expression::Prefix(PrefixOp::Negate, Box::new(rhs)))
+            }
+            _ => Err(Error::Unexpected),
+        }
+    }
+
+    /// Left denotation. This is the parse of the symbol when it
+    /// has an expression to the left hand side of it.
+    ///
+    /// This is responsible for parsing infix operators and function
+    /// calls.
+    fn led(&self, parser: &mut Parser, lhs: Expression) -> Result<Expression> {
+        use Expression::*;
+        match *self {
+
+            // Binary infix operator
+            Token::Equals |
+            Token::Plus |
+            Token::Minus |
+            Token::Star |
+            Token::Slash => {
+                let rhs = try!(parser.expression(self.lbp()));
+                let op = InfixOp::for_token(self).unwrap();
+                Ok(Infix(Box::new(lhs), op, Box::new(rhs)))
+            }
+
+            // Function call
+            Token::OpenBracket => {
+                let mut params = Vec::new();
+                while !parser.next_is(Token::CloseBracket) {
+                    let param = try!(parser.expression(0));
+                    params.push(param);
+                    if !parser.next_is(Token::CloseBracket) {
+                        try!(parser.advance_if(Token::Comma));
+                    }
+                }
+                try!(parser.advance_if(Token::CloseBracket));
+                Ok(Expression::Call(Box::new(lhs), params))
+            }
+
+            _ => Err(Error::Unexpected),
+        }
+    }
+}
+
+/// Parse error module. Contains the Result and Error types for the
+/// `parse` module.
+pub mod error {
+
+    /// Parser result type. Returned from parsing functions when
+    /// success can't be guaranteed.
+    pub type Result<T> = ::std::result::Result<T, Error>;
+
+    /// Parser error type. This distinguishes between the differen
+    /// kinds of errors that the `Parser` can encounter.
+    #[derive(Debug,PartialEq)]
+    pub enum Error {
+        /// Unexpected token.
+        Unexpected,
+
+        /// Incomplete data
+        Incomplete,
+    }
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Tokeniser, Parser};
+    use super::super::*;
+    use Expression::*;
+
+    macro_rules! check_parse {
+        ($src:expr, $expected:expr) => {
+            let src:&str = $src;
+            let tokeniser = Tokeniser::new_from_str(src);
+            let mut parser = Parser::new(tokeniser);
+            assert_eq!(Ok($expected), parser.expression(0));
+        }
+    }
+
+    #[test]
+    fn create_tokeniser_from_str_returns_tokeniser() {
+        let src = "hello = world";
+        Tokeniser::new_from_str(src);
+    }
+
+    #[test]
+    fn parse_simple_string() {
+        check_parse!("hello + 123",
+                     Infix(Box::new(Identifier("hello".to_string())),
+                           InfixOp::Add,
+                           Box::new(Literal(123))));
+    }
+
+    #[test]
+    fn parse_with_precedence() {
+        check_parse!("1 + 2 * 3",
+                     Infix(Box::new(Literal(1)),
+                           InfixOp::Add,
+                           Box::new(Infix(Box::new(Literal(2)),
+                                          InfixOp::Mul,
+                                          Box::new(Literal(3))))));
+    }
+
+    #[test]
+    fn parse_prefix_expressions() {
+        check_parse!("+1 * -2 + +3",
+                     Infix(Box::new(Infix(Box::new(Literal(1)),
+                                          InfixOp::Mul,
+                                          Box::new(Prefix(PrefixOp::Negate,
+                                                          Box::new(Literal(2)))))),
+                           InfixOp::Add,
+                           Box::new(Literal(3))));
+    }
+
+    #[test]
+    fn parse_simple_call() {
+        check_parse!("foo()",
+                     Call(Box::new(Identifier("foo".to_string())), Vec::new()));
+    }
+
+    #[test]
+    fn parse_complex_call() {
+        check_parse!("hello(1, 1 + 23, -world)",
+                     Call(Box::new(Identifier("hello".to_string())),
+                          vec![Literal(1),
+                               Infix(Box::new(Literal(1)), InfixOp::Add, Box::new(Literal(23))),
+                               Prefix(PrefixOp::Negate,
+                                      Box::new(Identifier("world".to_string())))]));
+
     }
 }
