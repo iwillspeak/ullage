5fc9516633c68f7e2e258cc646db10bf797941c1
Will Speak <lithiumflame@gmail.com>
2017-04-16 10:37:43 +0100

    TokenIterator Refactorings
    Factor out some common parts of the iterator state machine.

--- src/syntax/parse/token_iterator.rs
+++ src/syntax/parse/token_iterator.rs
@@ -26,6 +26,11 @@ impl<'a, S> TokenIterator<'a, S>
         }
     }
 
+    /// Skip Past Matching Chars
+    ///
+    /// Searches through the source for the first source position that
+    /// doesn't match the given predicate function. Used to find the
+    /// end of longer tokens such as whitespace or identifiers.
     fn skip_src_past<P>(&mut self, mut pos: S::Pos, pred: P) -> S::Pos
         where P: Fn(char) -> bool
     {
@@ -38,65 +43,64 @@ impl<'a, S> TokenIterator<'a, S>
         pos
     }
 
+    /// Optional Two-Char Token
+    ///
+    /// Peeks at the next character. If it matches the expected second
+    /// part of a two-char token then `two` is returned. Otherwise
+    /// `one` is returned instead.
+    fn opt_two_char(&mut self, second: char, two: Token<'a>, one: Token<'a>) -> Token<'a> {
+        match self.src.take(self.idx) {
+            Some((ch, p)) if ch == second => {
+                self.idx = p;
+                two
+            }
+            _ => one,
+        }
+    }
+
     /// Retrieve the next 'raw' token. This is the next lexical match
     /// in the buffer, and may include whitespace and other trivia
     /// tokens.
     fn next_raw(&mut self) -> Option<Token<'a>> {
 
         let ts = self.idx;
-        self.src.take(ts).and_then(|(c, pos)| {
+        self.src.take(ts).map(|(c, pos)| {
             self.idx = pos;
             match c {
-                '=' => {
-                    match self.src.take(pos) {
-                        Some(('=', p)) => {
-                            self.idx = p;
-                            Some(Token::DoubleEquals)
-                        }
-                        _ => Some(Token::Equals),
-                    }
-                }
-                '!' => {
-                    match self.src.take(pos) {
-                        Some(('=', p)) => {
-                            self.idx = p;
-                            Some(Token::BangEquals)
-                        }
-                        _ => Some(Token::Bang),
-                    }
-                }
-                '+' => Some(Token::Plus),
-                '-' => Some(Token::Minus),
-                '*' => Some(Token::Star),
-                '/' => Some(Token::Slash),
-                '(' => Some(Token::OpenBracket),
-                ')' => Some(Token::CloseBracket),
-                '[' => Some(Token::OpenSqBracket),
-                ']' => Some(Token::CloseSqBracket),
-                ',' => Some(Token::Comma),
-                ':' => Some(Token::Colon),
-                '<' => Some(Token::LessThan),
-                '>' => Some(Token::MoreThan),
+                '=' => self.opt_two_char('=', Token::DoubleEquals, Token::Equals),
+                '!' => self.opt_two_char('=', Token::BangEquals, Token::Bang),
+                '+' => Token::Plus,
+                '-' => Token::Minus,
+                '*' => Token::Star,
+                '/' => Token::Slash,
+                '(' => Token::OpenBracket,
+                ')' => Token::CloseBracket,
+                '[' => Token::OpenSqBracket,
+                ']' => Token::CloseSqBracket,
+                ',' => Token::Comma,
+                ':' => Token::Colon,
+                '<' => Token::LessThan,
+                '>' => Token::MoreThan,
                 '#' => {
                     self.idx = self.skip_src_past(pos, |c| c != '\n');
-                    Some(Token::Whitespace(self.src.slice(ts, self.idx)))
+                    Token::Whitespace(self.src.slice(ts, self.idx))
                 }
                 '0'...'9' => {
                     self.idx = self.skip_src_past(pos, |c| c >= '0' && c <= '9');
                     let token_str = self.src.slice(ts, self.idx);
                     // we have cheked that it's a valid numeric literal,
-                    // so unwrap is fine here.
-                    Some(Token::Literal(token_str.parse::<i64>().unwrap()))
+                    // so unwrap is fine here.||||
+                    Token::Literal(token_str.parse::<i64>().unwrap())
                 }
                 c if c.is_alphabetic() || c == '_' => {
                     self.idx = self.skip_src_past(pos, |c| c.is_alphanumeric() || c == '_');
-                    Some(Token::Word(&self.src.slice(ts, self.idx)))
+                    Token::Word(&self.src.slice(ts, self.idx))
                 }
                 c if c.is_whitespace() => {
                     self.idx = self.skip_src_past(pos, |c| c.is_whitespace());
-                    Some(Token::Whitespace(&self.src.slice(ts, self.idx)))
+                    Token::Whitespace(&self.src.slice(ts, self.idx))
                 }
-                _ => Some(Token::Unknown(c)),
+                _ => Token::Unknown(c),
             }
         })
     }
