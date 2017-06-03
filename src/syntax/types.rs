e9844a6002a74eba276fd58b51aded87a208f93c
Will Speak <lithiumflame@gmail.com>
2017-06-03 18:42:48 +0100

    Move Types to Their Own Module

---
+++ src/syntax/types.rs
@@ -0,0 +1,55 @@
+//! Syntax Types
+//!
+//! This module contians the structures used in the syntax tree to
+//! reference types.
+
+/// Type Reference
+///
+/// Represents a reference to a type. This could be a simple reference
+/// to a named type or a complex type such as an array or Tuple.
+#[derive(Debug,PartialEq)]
+pub enum TypeRef {
+    Simple(String),
+    Unit,
+    Tuple(Vec<TypeRef>),
+    Array(Box<TypeRef>),
+}
+
+impl TypeRef {
+    /// Create a New Simple Type
+    ///
+    /// A simple type is a direct reference to a non-generic non-array
+    /// type, such as `Num` or `String`.
+    pub fn simple(name: &str) -> Self {
+        TypeRef::Simple(String::from(name))
+    }
+
+    /// Create a new Unit Type Reference
+    ///
+    /// The unit type is represented as a struct with no contents. It
+    /// has special meaning in some areas as it can be used to idicate
+    /// the absence of a value.
+    pub fn unit() -> Self {
+        TypeRef::Unit
+    }
+
+    /// Create a Tuple Type
+    ///
+    /// A tuple type is an ordered collection of values. Each value
+    /// can be of a different type.
+    pub fn tuple(inner: Vec<TypeRef>) -> Self {
+        if inner.len() == 0 {
+            Self::unit()
+        } else {
+            TypeRef::Tuple(inner)
+        }
+    }
+
+    /// Create an Array Type
+    ///
+    /// An array type represents a contiguous collection of another
+    /// type.
+    pub fn array(inner: TypeRef) -> Self {
+        TypeRef::Array(Box::new(inner))
+    }
+}
