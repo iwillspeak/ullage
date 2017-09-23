713b90bcbc5819ae5451010d67428ea81fec549b
Will Speak <lithiumflame@gmail.com>
2017-09-23 18:23:50 +0100

    Switch to Python for Tasks
    Move from a `Rakefile` to a suite of invoke tests in python. The plan
    is to eventually no longer require both Ruby *and* Python.

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+@task
+def clean(ctx):
+    ctx.run("rm -f a.out")
+    ctx.run("rm -rf specbin/")
+    ctx.run("cargo clean")
+
+@task(default=True)
+def build(ctx):
+    ctx.run("cargo build")
+
+@task
+def docs(ctx):
+    with ctx.cd("docs/"):
+        ctx.run("d")
+
+@task
+def test(ctx):
+    ctx.run("cargo test")
+    ctx.run("ruby specs.rb")
