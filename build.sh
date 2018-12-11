fab264a651f7512979bf7c0e429f9a7188f23aba
Will Speak <lithiumflame@gmail.com>
2018-12-11 07:15:44 +0000

    Setup Build Environment if Missing
    Fixup the build environment if there isn't one already.

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -d ${DIR}/venv ]
+then
+	virtualenv venv
+	pip install -r ${DIR}/requirements.txt
+fi
+
 source ${DIR}/venv/bin/activate
 
 invoke "$@"
