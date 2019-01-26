1e7f331b527f55f6057cae915c98f57a0dd65482
Will Speak <lithiumflame@gmail.com>
2019-01-26 10:38:16 +0000

    Add Benchmarks Support
    Files in the `spec/bench/` folder can be built with optimisations
    enabled and benchmakred with the `invoke bench` task.

--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
 from invoke import task
+import os
+import glob
 
 @task
 def clean(ctx):
@@ -7,8 +9,9 @@ def clean(ctx):
     ctx.run("cargo clean")
 
 @task(default=True)
-def build(ctx):
-    ctx.run("cargo build")
+def build(ctx, release=True):
+    mode = "--release" if release else "--debug"
+    ctx.run("cargo build " + mode)
 
 @task
 def docs(ctx):
@@ -19,3 +22,18 @@ def docs(ctx):
 def test(ctx):
     ctx.run("cargo test")
     ctx.run("python specs.py")
+
+@task(build)
+def bench(ctx, opt_level=3):
+    for bench in glob.glob("spec/bench/*.ulg"):
+        output = bench.lstrip('spec/').rstrip('.ulg')
+        output = os.path.join("specbin", "bench", output)
+        try:
+            os.makedirs(os.path.dirname(output))
+        except OSError:
+            pass
+        print "bench={0}, output={1}, opt={2}".format(bench, output, opt_level)
+        ctx.run("target/release/ullage {0} -O{1} -o {2}"
+                .format(bench, opt_level, output))
+        ctx.run("time {0}".format(output))
+
