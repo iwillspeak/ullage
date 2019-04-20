d267cbb8000b680a2f9e7096e4bd43f84a51babb
Will Speak <lithiumflame@gmail.com>
2019-04-20 11:45:32 +0100

    Check for Valid Prequisites in Build
    Make sure we have the right version of python installed. Use python3
    when creating a virtual envrionment.

--- build.sh
+++ build.sh
@@ -4,9 +4,19 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function check_for()
+{
+	command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 is required but it's not installed.  Aborting."; exit 1; }
+}
+
+check_for python3
+check_for pip3
+check_for virtualenv
+
 if [ ! -d ${DIR}/venv ]
 then
-	virtualenv venv
+	# make sure to use a Python3 interpreter
+	virtualenv -p python3 venv
 fi
 
 source ${DIR}/venv/bin/activate
